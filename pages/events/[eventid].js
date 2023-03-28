4d39da55adb05157f240947b37fc584668da918b
alleycaaat <achulslander@gmail.com>
2023-03-28 15:41:31 -0500

    init commit

---
+++ pages/events/[eventid].js
@@ -0,0 +1,9 @@
+const EventDetail = () => {
+    return (
+        <section>
+            <h1>Event Details</h1>
+        </section>
+    );
+};
+
+export default EventDetail;
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+const AllEvents = () => {
+    return (
+        <section>
+            <h1>All Events</h1>
+        </section>
+    );
+};
+
+export default AllEvents;
